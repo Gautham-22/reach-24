27152e83abb5b5fd5cf5258b40b766503d8f544c
Gautham-22 <gautham22poy@gmail.com>
2024-02-11 15:11:57 +0530

    navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FaPlus } from 'react-icons/fa';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="logo">
+                <div>
+                    <img src={'ig_short.png'} alt="Instagram" />
+                </div>
+                <div>
+                    <img src={'ig_long.png'} alt="Instagram" />
+                </div>
+            </div>
+            <div className="icons">
+                <div>
+                    <img src={'home.png'} alt="Home" />
+                </div>
+                <div>
+                    <img src={'search.png'} alt="Search" />
+                </div>
+                <div>
+                    <img src={'heart.png'} alt="Like" />
+                </div>
+                <div>
+                    <img src={'send.png'} alt="DM" />
+                </div>
+            </div>
+            <button type="button" className="post-btn">
+                <FaPlus /> <p>Add new post</p>
+            </button>
+        </div>
+    )
+}
+
+export default Navbar
